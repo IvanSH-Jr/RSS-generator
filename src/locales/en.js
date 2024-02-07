4d4eba4ec74f37e878cba2cea9be415ac8e86ff9
Ivan Shadrin <shadrin@spbec-mining.ru>
2024-02-07 18:36:04 +0300

    fix(en.js): fix en local

--- src/locales/en.js
+++ src/locales/en.js
@@ -9,10 +9,22 @@ setLocale({
 export default {
   translation: {
     appName: 'RSS aggregator',
-    rssInputPlaceholde: 'RSS link',
+    rssForm: {
+      inputPlaceholder: 'RSS link',
+      finished: 'RSS successfully uploaded',
+      sending: 'The request is being sent',
+      sent: 'RSS is loading',
+      error: {
+        invalid: 'The resource does not contain valid RSS',
+        duplicate: 'RSS already exists',
+        errUrl: 'The link must be a valid URL',
+        'Network Error': 'Network Error',
+      },
+    },
     lead: 'Start reading RSS today! It\'s easy, it\'s beautiful.',
     example: 'Example: https://ru.hexlet.io/lessons.rss',
-    errUrl: 'The link must be a valid URL',
-
+    feeds: 'Feeds',
+    postList: 'Posts',
+    readBtn: 'Show',
   },
 };
